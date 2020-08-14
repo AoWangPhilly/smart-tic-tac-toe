138c4c147be5be1701409aa176443bec89941adc
AoWangDrexel <aw3338@drexel.edu>
2020-08-14 19:34:26 -0400

    Fix bug in situation where there's a tie

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "TicTacToe.h"
+#include <math.h>
+
+int minMax(TicTacToe board, int depth, bool isMaximizing);
+char bestMove();
 
 int main()
 {
@@ -10,27 +15,71 @@ int main()
     TicTacToe game;
     game.display();
 
-    int turns = 0;
+    std::string winner;
     char pos;
-    while (!game.checkWin())
+    bool draw = true;
+
+    /*  Loops through the game and will end under two conditions:
+     *  - There aren't any moves left if the board is full => tie
+     *  - A player wins
+     */
+    for (int turn = 0; turn < 9; ++turn)
     {
-        turns % 2 == 0
+        // Breaks if won
+        if (game.checkWin())
+        {
+            draw = false;
+            winner = (turn - 1) % 2 == 0 ? "Player 1 (X)" : "Player 2 (O)";
+            break;
+        }
+
+        // Even player (X), odd player (O)
+        turn % 2 == 0
             ? std::cout << "Player 1 (X) make your move: \n"
             : std::cout << "Player 2 (O) make your move: \n";
         std::cin >> pos;
+
+        // Loops continuously until player inputs unfilled space
         while (!game.isAvailable(pos))
         {
             std::cout << "Position taken, try another spot!\n";
             std::cin >> pos;
         }
-        turns % 2 == 0 ? game.place(pos, 'X') : game.place(pos, 'O');
+
+        turn % 2 == 0 ? game.place(pos, 'X') : game.place(pos, 'O');
         game.display();
-        ++turns;
     }
 
-    turns % 2 == 0
-        ? std::cout << "Congratulations Player 1 (O)!\n"
-        : std::cout << "Congratulations Player 2 (X)!\n";
+    if (draw)
+    {
+        std::cout << "Tie Game! Try again?\n";
+    }
+    else
+    {
+        std::cout << "Congratulations " << winner << "!\n";
+    }
 
     return 0;
+}
+
+int minMax(TicTacToe board, int depth, bool isMaximizing)
+{
+    if (!depth || board.checkWin())
+    {
+        return -1;
+    }
+    if (isMaximizing)
+    {
+        int maxEval = -INFINITY;
+    }
+    else
+    {
+        int minEval = INFINITY;
+    }
+    return 0;
+}
+
+char bestMove()
+{
+    return '1';
 }
