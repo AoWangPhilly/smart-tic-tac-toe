ca070cfb4eaa749fa6dc3320c01fb6bff401ebfd
AoWangDrexel <aw3338@drexel.edu>
2020-08-26 22:52:50 -0400

    Implement game and minimax algorithm

--- src/python/main.py
+++ src/python/main.py
@@ -1,13 +1,87 @@
-from tictactoe import TicTacToe, Move
+'''
+
+'''
+
+from tictactoe import TicTacToe
+import re
+import sys
+
 
 def miniMax(board, depth, isMaximizing):
+    winner = board.checkWin()
+    if winner == 'X':
+        return 10
+    elif winner == 'O':
+        return -10
+    elif winner == 'tie':
+        return 0
+
+    if isMaximizing:
+        bestVal = -sys.maxsize - 1
+        for pos in board.getFreeSpace():
+            spot = board.getBoard()[pos[0]][pos[1]]
+            board.place('X', pos)
+            val = miniMax(board, depth + 1, False)
+            board.place(spot, pos)
+
+            bestVal = max(bestVal, val)
+        return bestVal
+    else:
+        bestVal = sys.maxsize
+        for pos in board.getFreeSpace():
+            spot = board.getBoard()[pos[0]][pos[1]]
+            board.place('O', pos)
+            val = miniMax(board, depth + 1, True)
+            board.place(spot, pos)
+
+            bestVal = min(bestVal, val)
+        return bestVal
+
+
+def miniMaxAB(board, depth, isMaximizing, alpha=sys.maxsize, beta=-sys.maxsize - 1):
     pass
 
-def miniMaxAB(board, depth, isMaximizing, alpha, beta):
+
+def getBestMove(board):
+    bestVal = -sys.maxsize - 1
+    
+    for pos in board.getFreeSpace():
+        spot = board.getBoard()[pos[0]][pos[1]]
+        board.place('X', pos)
+        moveVal = miniMax(board, 0, False)
+        board.place(spot, pos)
+        if moveVal > bestVal:
+            bestMove = pos
+            bestVal = moveVal
+
+    return bestMove
+
+
+def getBestMoveAB(board):
     pass
+
 
 if __name__ == '__main__':
     game = TicTacToe()
-    for i in range(9):
-        pass
-    print('Hello World!')
+    print(game)
+    for turn in range(9):
+        pos = '{} {}'.format(getBestMove(game)[0], getBestMove(game)[1]) if turn % 2 == 0 else input(
+            'Make your move, Player 2 (O), i.e. "0 0": ')
+
+        pos = tuple(map(int, pos.split(' '))) 
+
+        while not game.isAvailable(pos):
+            pos = input(
+                '[Row: {}, Col: {}] is taken! Try again?, i.e. "0 0": '.format(pos[0], pos[1]))
+            pos = tuple(map(int, pos.split(' ')))
+
+        game.place('X', pos) if turn % 2 == 0 else game.place('O', pos)
+        print(game)
+        won = game.checkWin()
+        if won:
+            print('Congratulations Player 1 (X)') if won == 'X' else print(
+                'Congratulations Player 2 (O)')
+            break
+    else:
+        if not game.movesLeft():
+            print('Tie game!! Try again? :D')
--- src/python/tictactoe.py
+++ src/python/tictactoe.py
@@ -1,11 +1,17 @@
+'''
+
+'''
 class TicTacToe():
+    '''TicTacToe class
+    '''
     def __init__(self):
         self.board = [list(range(i, i+3)) for i in range(1, 10, 3)]
 
     def __str__(self):
         board = '     |     |     \n'
         for i in range(3):
-            board += '  {}  |  {}  |  {}\n'.format(self.board[i][0], self.board[i][1], self.board[i][2])
+            board += '  {}  |  {}  |  {}\n'.format(   
+                self.board[i][0], self.board[i][1], self.board[i][2])
             if i < 2:
                 board += '_____|_____|_____\n     |     |     \n'
         return board + '     |     |     \n\n'
@@ -17,16 +23,22 @@ class TicTacToe():
         self.board = board
 
     def isAvailable(self, pos):
+        '''
+        '''
         for i in range(3):
             for j in range(3):
-                if self.board[pos.x][pos.y] not in 'XO':
+                if str(self.board[pos[0]][pos[1]]) not in 'XO':
                     return True
         return False
 
-    def place(self, pos, mark):
-        self.board[pos.x][pos.y] = mark
+    def place(self, mark, pos):
+        '''
+        '''
+        self.board[pos[0]][pos[1]] = mark
 
     def movesLeft(self):
+        '''
+        '''
         for i in range(3):
             for j in range(3):
                 if type(self.board[i][j]) == int:
@@ -34,9 +46,13 @@ class TicTacToe():
         return False
 
     def getFreeSpace(self):
-        return [Move(i, j) for i in range(3) for j in range(3) if type(self.board[i][j]) == int]
+        '''
+        '''
+        return [(i, j) for i in range(3) for j in range(3) if type(self.board[i][j]) == int]
 
     def checkWin(self):
+        '''
+        '''
         for i in range(3):
             if self.board[0][i] == self.board[1][i] == self.board[2][i]:
                 return self.board[0][i]
@@ -51,18 +67,3 @@ class TicTacToe():
 
         if not self.movesLeft():
             return 'tie'
-
-
-class Move():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return '({}, {})'.format(self.x, self.y)
-
-    def getMove(self):
-        return Move(self.x, self.y)
-
-    def setMove(self, x, y):
-        self.Move = Move(x, y)
